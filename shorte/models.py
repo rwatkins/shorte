45f163d7e10ba06988bc32b671c006b491fed2b6
Riley Watkins <riley@rileywatkins.com>
2011-08-30 17:57:29 -0700

    Initialize url entry with just long_url and set hits to 0

--- shorte/models.py
+++ shorte/models.py
@@ -1,5 +1,6 @@
 from shorte import app
 from flaskext.sqlalchemy import SQLAlchemy
+import urlencoder
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/shorte.db'
 db = SQLAlchemy(app)
@@ -10,6 +11,6 @@ class Shorte(db.Model):
     long_url = db.Column(db.String(255), unique=True)
     hits = db.Column(db.Integer)
 
-    def __init__(self, short_url, long_url):
-        self.short_url = short_url
+    def __init__(self, long_url):
         self.long_url = long_url
+        self.hits = 0
