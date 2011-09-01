a2bc13cf4e6c9f052ff06da9073e4d51b1ec3c94
Riley Watkins <riley@rileywatkins.com>
2011-09-01 00:42:52 -0700

    Change 'hits' to 'clicks'

--- shorte/models.py
+++ shorte/models.py
@@ -8,8 +8,8 @@ class Shorte(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     short_url = db.Column(db.String(20), unique=True)
     long_url = db.Column(db.String(255), unique=True)
-    hits = db.Column(db.Integer)
+    clicks = db.Column(db.Integer)
 
     def __init__(self, long_url):
         self.long_url = long_url
-        self.hits = 0
+        self.clicks = 0
